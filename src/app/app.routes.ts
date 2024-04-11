6b65fd674e900d51685d6339b338bc39b997ab06
Ritin Georgeḍ <ritingeorge.d@gmail.com>
2024-04-12 01:30:26 +0530

    refactor: move register course inside home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from "@angular/router";
-import { AppComponent } from "./app.component";
-import { UploadCourseListComponent } from "./pages/register-course/upload-course-list/upload-course-list.component";
-import { RegisterCourseComponent } from "./pages/register-course/register-course.component";
+import { UploadCourseListComponent } from "./pages/home/register-course/upload-course-list/upload-course-list.component";
+import { RegisterCourseComponent } from "./pages/home/register-course/register-course.component";
 import { HomeComponent } from "./pages/home/home.component";
 
 export const routes: Routes = [
